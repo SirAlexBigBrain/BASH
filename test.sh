89db9d52a3cd9f3d1e5d5383a12d9cae22b0e007
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-19 15:05:56 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,18 @@ echo "hi there $name!"
 read -p "What is your name?" " Alex "
 echo "Hello $name"
 echo "Welcome to my code!"
+#!/bin/bash
+
+# Bash Scripting
+
+read -p "What is your name? " Wayne
+
+if [[ -z ${name} ]]
+then
+    echo "Please enter your name!"
+else
+    echo "Hi there ${name}"
+fi
 chmod +x main.sh
 ./main.sh
 True if file exists
