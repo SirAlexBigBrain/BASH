ea94d7575472f2e99b4cea656b0b222daba1a49c
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-20 12:04:30 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,3 +10,13 @@ sudo su
 apt update
 apt install docker
 logout
+VAR="Welcome to the bash scripting"
+echo $VAR
+
+num1=10
+# sudo p kill user Hardwaregore
+num2=20
+#calculating?
+num3=$(($num1+$num2))
+#I'm trying
+echo "The sum is:$num3"
