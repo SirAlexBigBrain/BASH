230c2f24fde7d3e57f0bffa1bb4c954593717ac3
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-19 20:36:31 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name="Technodot & Hardwaregore"
 echo "hi there $name!"
-read -p "What is your name?" " Alex "
+read -p "What is your name?" "HUMAN"
 echo "Hello $name"
 echo "Welcome to my code!"
 #!/bin/bash
