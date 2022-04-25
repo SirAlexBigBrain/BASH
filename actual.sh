1e80c69186ce6c310ec3d77d0cb130c3dac89661
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-25 14:57:06 -0500

    Update actual.sh

--- actual.sh
+++ actual.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+name="SirAlexBigBrain"
+echo "hello"
+echo "what be your name?"
+echo ${name}
+chmod +x main.sh
+./main.sh
+echo "my name is $1"
+sudo su
+apt update
+apt install docker
+logout
+VAR="Welcome to the bash scripting"
+echo $VAR
 
+num1=10
+# sudo p kill user Hardwaregore
+num2=20
+#calculating?
+num3=$(($num1+$num2))
+#I'm trying
+echo "The sum is: $num3"
